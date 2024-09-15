71a5cf947099c74451303194f224f94b33dceb3b
Muhammadyasakhan <153687527+Muhammadyasakhan@users.noreply.github.com>
2024-09-16 03:04:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+// chap 1
+
+//alert("Error! Please enter a valid Password"); 
+
+// alert("Welcome to JS Land...\n Happy cooding!"); 
+
+// alert("Welcome to JS Land...\n "); 
+
+// alert("Happy cooding!"); 
+
+// chap 2
+
+//q1 
+//var username;
+
+//q2
+//var myName = "Muhammad Yasa";
+
+//q3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+//q4
+// var studentName = "Muhammad Yasa";
+// var studentAge = 20;
+// var studentGrade = "A";
+// var studentCity = "Krachi";
+
+// alert("Student Name: " + studentName + "\nStudent Age: " + studentAge + "\nStudent Grade: " + studentGrade + "\nStudent City: " + studentCity);
+
+// q5
+// var pizza = "Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+
+// q6
+// var email = "My Email Address Is muhammayasa307@gmail.com"
+// alert(email)
+
+//q7
+// var message;
+// message = "I am tryin to learn from a book A smater way to learn java script !";
+// alert(message);
+
+// chap 3
+
+// q1
+//var age 
+//age = "My age is 19 years old."
+//alert(age);
+
+//q3
+// var birthYear 
+// birthYear = "My birth year is 2004"
+// document.write(birthYear)
+
+// chap 4
+
+// let a, b, c;
+
+
+// let illegal;  
+// let my-variable;  
+// let my variable;  
+// let myVar;  
+// let let;  
+
+
+// let myVar;
+// let _name;
+// let $value;
+// let camelCase;
+// let camel_case;
+
+// // chap 5
+
+// let num1 = 5;
+// let num2 = 3;
+// let sum = num1 + num2;
+// document.write("The sum is: " + sum);
+
+// let num1 = 10;
+// let num2 = 4;
+// let subtract = num1 - num2;
+// let multiply = num1 * num2;
+// let divide = num1 / num2;
+// let modulus = num1 % num2;
+
+// document.write("<h2>Multiplication Table of 4</h2>");
+// for (let i = 1; i <= 10; i++) {
+//   document.write("4 × " + i + " = " + (4 * i) + "<br>");
+// }
